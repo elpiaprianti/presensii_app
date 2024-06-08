9a91a15bfbc2dfaa58a3b7302ccda68064efe7ed
elpi-ksi <elpiaprianti@gmail.com>
2024-06-08 23:45:30 +0700

    https://github.com/elpiaprianti/presensii_app/commits/main/

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:presensi_app/screen/attandance_recap_screen.dart';
-import 'package:presensi_app/screen/dashboard_screen.dart';
-import 'package:presensi_app/screen/login_screen.dart';
 import 'package:presensi_app/screen/splash_screen.dart';
+import 'package:intl/date_symbol_data_local.dart'; 
 
-void main() {
+Future<void> main() async {
+  //untuk format tanggal indonesia
+  await initializeDateFormatting('id', null);
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
